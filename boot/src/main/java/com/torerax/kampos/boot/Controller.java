63ea8ca8da5ac8b94ea9af7e34912d94aa949fef
Pedro <pedrotorerax@gmail.com>
2024-01-04 20:54:52 +0100

    test: check coverage out jacoco

--- boot/src/main/java/com/torerax/kampos/boot/Controller.java
+++ boot/src/main/java/com/torerax/kampos/boot/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
         System.out.println("prueba 3");
         System.out.println("prueba 4");
         System.out.println("prueba 5");
+
         System.out.println("prueba 6");
     }
 }
