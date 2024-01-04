429eb5f3df5e19ff91f028fc333fba6998315db5
Pedro <pedrotorerax@gmail.com>
2024-01-04 21:08:56 +0100

    test: resolve test

--- boot/src/main/java/com/torerax/kampos/boot/Controller.java
+++ boot/src/main/java/com/torerax/kampos/boot/Controller.java
@@ -1,22 +1,14 @@
 package com.torerax.kampos.boot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class Controller {
 
-    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-
-    public void pruebaTest() {
-        logger.info("prueba 1");
-        logger.info("3 2");
-        logger.info("D 3");
-        logger.info("prueba 4");
-        logger.info("ASD 5");
-        logger.info("prueba 5");
-        logger.info("D 5");
-        logger.info("pru1eba 5");
-
-        logger.info("13 6");
+    public void pruebaTest () {
+        System.out.println("prueba 1");
+        System.out.println("3 2");
+        System.out.println("D 3");
+        System.out.println("prueba 4");
+        System.out.println("ASD 5");
     }
 }
--- boot/src/main/java/com/torerax/kampos/boot/ControllerPart.java
+++ boot/src/main/java/com/torerax/kampos/boot/ControllerPart.java
@@ -1,22 +1,11 @@
 package com.torerax.kampos.boot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ControllerPart {
-
-    private static final Logger logger = LoggerFactory.getLogger(ControllerPart.class);
-
-    public void pruebaTest() {
-        logger.info("123 1");
-        logger.info("prueba 2");
-        logger.info("prueba 3");
-        logger.info("EE 4");
-        logger.info("prueba 5");
-        logger.info("D 5");
-        logger.info("DD 5");
-        logger.info("HJ 5");
-
-        logger.info("IUYK 6");
+    public void pruebaTest () {
+        System.out.println("123 1");
+        System.out.println("prueba 2");
+        System.out.println("prueba 3");
+        System.out.println("EE 4");
+        System.out.println("prueba 5");
     }
 }
