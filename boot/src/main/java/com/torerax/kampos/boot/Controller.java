225af0ba9546f57e159d5131cf8cb8b8d043693c
Pedro <pedrotorerax@gmail.com>
2024-01-04 21:03:11 +0100

    test: check coverage with jacoco

--- boot/src/main/java/com/torerax/kampos/boot/Controller.java
+++ boot/src/main/java/com/torerax/kampos/boot/Controller.java
@@ -5,14 +5,14 @@ public class Controller {
 
     public void pruebaTest () {
         System.out.println("prueba 1");
-        System.out.println("prueba 2");
-        System.out.println("prueba 3");
+        System.out.println("3 2");
+        System.out.println("D 3");
         System.out.println("prueba 4");
+        System.out.println("ASD 5");
         System.out.println("prueba 5");
-        System.out.println("prueba 5");
-        System.out.println("prueba 5");
-        System.out.println("prueba 5");
+        System.out.println("D 5");
+        System.out.println("pru1eba 5");
 
-        System.out.println("prueba 6");
+        System.out.println("13 6");
     }
 }
--- boot/src/main/java/com/torerax/kampos/boot/ControllerPart.java
+++ boot/src/main/java/com/torerax/kampos/boot/ControllerPart.java
@@ -2,15 +2,15 @@ package com.torerax.kampos.boot;
 
 public class ControllerPart {
     public void pruebaTest () {
-        System.out.println("prueba 1");
+        System.out.println("123 1");
         System.out.println("prueba 2");
         System.out.println("prueba 3");
-        System.out.println("prueba 4");
+        System.out.println("EE 4");
         System.out.println("prueba 5");
-        System.out.println("prueba 5");
-        System.out.println("prueba 5");
-        System.out.println("prueba 5");
+        System.out.println("D 5");
+        System.out.println("DD 5");
+        System.out.println("HJ 5");
 
-        System.out.println("prueba 6");
+        System.out.println("IUYK 6");
     }
 }
