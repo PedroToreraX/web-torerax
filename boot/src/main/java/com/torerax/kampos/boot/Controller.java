fa2f4ae36a9ef51917c19bd9cf84d466fa1d6fdf
Pedro <pedrotorerax@gmail.com>
2024-01-04 20:47:25 +0100

    test: test controller

---
+++ boot/src/main/java/com/torerax/kampos/boot/Controller.java
@@ -0,0 +1,11 @@
+package com.torerax.kampos.boot;
+
+
+public class Controller {
+
+    public void pruebaTest () {
+        System.out.println("prueba 1");
+        System.out.println("prueba 2");
+        System.out.println("prueba 3");
+    }
+}
